0f231b26360c4d9cdc63a94475494a04c86f220a
Jacob Pixler <jacobpixlerdev@gmail.com>
2024-04-15 17:53:23 -0600

    fired up the flux capacitor

--- src/pages/api/email/index.ts
+++ src/pages/api/email/index.ts
@@ -3,7 +3,8 @@ import nodemailer from "nodemailer";
 import type Mail from "nodemailer/lib/mailer";
 
 export default async function handler(request: NextRequest) {
-  const { email, name, message } = JSON.parse(request.body);
+  const body = JSON.stringify(request.body);
+  const { email, name, message } = JSON.parse(body);
   console.log("\x1b[36m%s\x1b[0m", email);
 
   const transport = nodemailer.createTransport({
